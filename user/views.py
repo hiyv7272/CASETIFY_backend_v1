bd5888516f1ffa505cf186e6aef7e0d3e498610e
inyong Pang <hiyv7272@gmail.com>
2020-08-26 18:46:29 +0900

    User app Refactoring - user/myshippinginfo update - reponse data modify

--- user/views.py
+++ user/views.py
@@ -7,10 +7,10 @@ from django.views import View
 from django.http import JsonResponse, HttpResponse
 from django.db import transaction
 from casetify_backend.settings import SECRET_KEY
+from .utils import login_decorator, ValidateData
 
 from .models import User
 from order.models import Orderer
-from .utils import login_decorator, ValidateData
 
 
 class SignUpView(View):
@@ -19,8 +19,8 @@ class SignUpView(View):
 
             try:
                 data = json.loads(request.body)
-
                 validate_data = ValidateData(data)
+
                 validated_password = validate_data.password()
                 if validated_password:
                     return validated_password
@@ -44,60 +44,36 @@ class SignUpView(View):
                 return HttpResponse(status=200)
 
             except TypeError:
-                return JsonResponse({'message': 'INVALID_TYPE'}, status=400)
+                return JsonResponse({'message': "INVALID_TYPE"}, status=400)
             except KeyError:
-                return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
+                return JsonResponse({'message': "INVALID_KEYS"}, status=400)
 
 
 class SignInView(View):
     def post(self, request):
-        data = json.loads(request.body)
-
         try:
+            data = json.loads(request.body)
             user = User.objects.get(email=data['email'])
 
             if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
                 access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
                 return JsonResponse({'access_token': access_token.decode('utf-8')}, status=200)
 
-            return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
+            return JsonResponse({'message': "INVALID_PASSWORD"}, status=401)
+
         except User.DoesNotExist:
-            return JsonResponse({'message': 'INVALID_USER'}, status=400)
+            return JsonResponse({'message': "INVALID_USER"}, status=400)
         except KeyError:
-            return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
+            return JsonResponse({'message': "INVALID_KEYS"}, status=400)
 
 
 class MyProfileView(View):
     @login_decorator
-    def get(self, request):
-        user = User.objects.get(id=request.user.id)
-
-        try:
-            result = dict()
-            result['name'] = user.name
-            result['email'] = user.email
-            result['mobile_number'] = user.mobile_number
-            result['first_name'] = user.first_name
-            result['last_name'] = user.last_name
-            result['introduction'] = user.introduction
-            result['website'] = user.website_url
-            result['location'] = user.location
-            result['twitter'] = user.twitter
-            result['image'] = user.profile_image_url
-
-            return JsonResponse(result, status=200)
-
-        except User.DoesNotExist:
-            return JsonResponse({"message": "INVALID_USER"}, status=400)
-        except KeyError:
-            return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
-
-    @login_decorator
     def post(self, request):
-        data = json.loads(request.body)
-        user = User.objects.get(id=request.user.id)
-
         try:
+            data = json.loads(request.body)
+            user = User.objects.get(id=request.user.id)
+
             user.name = data['name']
             user.bio = data['introduction']
             user.website_url = data.get('website')
@@ -113,20 +89,46 @@ class MyProfileView(View):
         except KeyError:
             return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
 
+    @login_decorator
+    def get(self, request):
+
+        try:
+            user = User.objects.get(id=request.user.id)
+
+            user_profile = dict()
+            user_profile['name'] = user.name
+            user_profile['email'] = user.email
+            user_profile['mobile_number'] = user.mobile_number
+            user_profile['first_name'] = user.first_name
+            user_profile['last_name'] = user.last_name
+            user_profile['introduction'] = user.introduction
+            user_profile['website'] = user.website_url
+            user_profile['location'] = user.location
+            user_profile['twitter'] = user.twitter
+            user_profile['image'] = user.profile_image_url
+
+            return JsonResponse({'data': user_profile}, status=200)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': "INVALID_USER"}, status=400)
+        except KeyError:
+            return JsonResponse({'message': "INVALID_KEYS"}, status=400)
+
 
 class MyShippingInfoView(View):
     @login_decorator
     def post(self, request):
-        data = json.loads(request.body)
-        user = User.objects.get(id=request.user.id)
-
         try:
-            user.first_name = data['first_name']
-            user.last_name = data['last_name']
-            user.address = data['address']
-            user.zipcode = data['zipcode']
-            user.mobile_number = data['mobile_number']
-            user.save()
+            data = json.loads(request.body)
+            user = User.objects.get(id=request.user.id)
+            orderer = Orderer.objects.get(USER=user)
+
+            orderer.first_name = data['first_name']
+            orderer.last_name = data['last_name']
+            orderer.address = data['address']
+            orderer.zipcode = data['zipcode']
+            orderer.mobile_number = data['mobile_number']
+            orderer.save()
 
             return HttpResponse(status=200)
 
@@ -135,10 +137,30 @@ class MyShippingInfoView(View):
         except KeyError:
             return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
 
+    @login_decorator
+    def get(self, request):
+        try:
+            user = User.objects.get(id=request.user.id)
+            orderer = Orderer.objects.get(USER=user)
+
+            orderer_info = dict()
+            orderer_info['first_name'] = orderer.first_name
+            orderer_info['last_name'] = orderer.last_name
+            orderer_info['address'] = orderer.address
+            orderer_info['zipcode'] = orderer.zipcode
+
+            return JsonResponse({'data': orderer_info}, status=200)
+
+        except user.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=400)
+        except orderer.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status=400)
+        except KeyError:
+            return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
+
 
 class KakaologinView(View):
     def post(self, request):
-
         try:
             kakao_token = request.headers["Authorization"]
             print('kt', kakao_token)
@@ -169,4 +191,3 @@ class KakaologinView(View):
             return JsonResponse({"message": "INVALID_TOKEN"}, status=400)
         except kakao_token.DoesNotExist:
             return JsonResponse({'message': 'INVALID_TOKEN'}, status=400)
-
